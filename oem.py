f6f942cb9ccce39356927b25132432837e1d0792
helymenezes <helymenezes@hotmail.com>
2024-03-02 02:19:03 -0300

    versao_1.2

--- oem.py
+++ oem.py
@@ -1,10 +1,10 @@
-# oem.py
 import streamlit as st
 import pandas as pd
 
 def filter_oem():
     st.title('PRODUÇÃO ORDEM E MANUTENÇÃO')
     
+    @st.cache_data
     def read_dataframe_oem():
         df_oem = pd.read_excel(r'C:\Users\helym\projeto_python\dashboard\content\base_sip_Concluido.xlsx')
         del_status = ['TREINAMENTO', 'CADASTRO', 'TREINAMENTO']
@@ -19,29 +19,40 @@ def filter_oem():
         
         return df_oem
     
-    @st.cache
-    def application_filter(df, data_inicial, data_final):
+    def application_filter(df, tipo_value, origem_value, data_inicial, data_final):
         df_filtered = df[
-            (df['DTCONCLUSAO'] >= data_inicial) &
-            (df['DTCONCLUSAO'] <= data_final)
+            df["TIPO"].isin(tipo_value) &
+            df['ORIGEM'].isin(origem_value) &
+            (df['DATACONCLUSAO'] >= data_inicial) &
+            (df['DATACONCLUSAO'] <= data_final)
         ]
         return df_filtered
     
+    # Apresenta a tabela do dataframe do streamlit e aplica o check box
     def show_sheet(df_filtered):
         df_filtered['CHECKBOX'] = ['FALSE'] * len(df_filtered)
         st.dataframe(df_filtered[['CHECKBOX', 'NUMOS', 'NUMOCORRENCIA', 'UC', 'IDSIGFI', 'TIPOCAUSA', 
                                 'NOMECLIENTE', 'ROTA', 'STATUS', 'EXECUTOR', 'DTCONCLUSAO', 'LATLONCON','DESCADICIONALPROG']])
     
-    # Load the dataframe initially
+    # Carrega o dataframe para aplicação do filtro e joga ele na variável df.
     df = read_dataframe_oem()
     
     col01, col02 = st.columns(2)
-    data_inicial_filter = col01.date_input('Data Inicial:', value=df['DTCONCLUSAO'].min())
-    data_final_filter = col02.date_input('Data Final:', value=df['DTCONCLUSAO'].max())
+    data_inicial_filter = pd.to_datetime(col01.date_input('Data Inicial: ', value = df['DATACONCLUSAO'].min()))
+    #data_inicial_filter = col01.date_input('Data Inicial:', value=df['DTCONCLUSAO'].min())
+    data_final_filter = pd.to_datetime(col02.date_input('Data Final: ', value = df['DATACONCLUSAO'].max()))
+    #data_final_filter = col02.date_input('Data Final:', value=df['DTCONCLUSAO'].max())
+    
+    col_tipo, col_origem = st.columns(2)
+    tipo_options = sorted(list(df['TIPO'].unique()))
+    tipo_filter = col_tipo.multiselect("Tipo: ", tipo_options)
+    
+    origem_options = sorted(list(df['ORIGEM'].unique()))
+    origem_filter = col_origem.multiselect("Origem: ",origem_options)
     
     # Button to execute the filter and display the table
     if st.button('Executar Filtros'):
-        df_filtered = application_filter(df, data_inicial_filter, data_final_filter)
+        df_filtered = application_filter(df, tipo_filter, origem_filter, data_inicial_filter, data_final_filter)
         
         if df_filtered.empty:
             st.warning("Não foi encontrado o resultado pré-definido. Redefina os filtros e tente novamente")
